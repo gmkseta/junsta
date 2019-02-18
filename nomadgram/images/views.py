7fe7237689dc9ded4995fad94181ba99f2acf39c
gmkseta <gmkseta@gmail.com>
2019-02-18 22:50:46 +0900

    add serializer

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+#from django.shortcuts import render api를 위한거라서
 # Create your views here.
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from . import models, serializer
+
+class ListAllImages(APIView):
+
+    def get(self, request, format=None):
+
+        all_images = models.Image.objects.all() 
+
+        serializer = serializer.ImageSerializer(all_images, many=True)
+
+        return Response(data=serializer.data)
+
