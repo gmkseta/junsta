8ba75944d3b938aa34090ca308d52e9a2dff7261
gmkseta <gmkseta@gmail.com>
2019-03-06 04:56:58 +0900

    get list of following & followers

--- nomadgram/users/urls.py
+++ nomadgram/users/urls.py
@@ -4,9 +4,11 @@ from . import views
 
 app_name = "users"
 urlpatterns = [
-    path("explore", view=views.ExploresUsers.as_view(), name="detail"),
+    path("explore", view=views.ExploreUsers.as_view(), name="detail"),
     path("<user_id>/follow", view=views.FollowUser.as_view(), name="follow_user"),
     path("<user_id>/unfollow", view=views.UnFollowUser.as_view(), name="unfollow_user"),
-    path("<username>", view=views.UserProfile.as_view(), name="user_profile")
+    path("<username>", view=views.UserProfile.as_view(), name="user_profile"),
+    path("<username>/followers", view=views.UserFollowers.as_view(), name="user_followers"),
+    path("<username>/following", view=views.UserFollowing.as_view(), name="user_following")
 
 ]
