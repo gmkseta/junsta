1f74e67af1c0666ad9462a7266c00edbc0a29547
gmkseta <gmkseta@gmail.com>
2019-03-10 01:50:58 +0900

    add user search

--- nomadgram/users/urls.py
+++ nomadgram/users/urls.py
@@ -7,8 +7,10 @@ urlpatterns = [
     path("explore", view=views.ExploreUsers.as_view(), name="detail"),
     path("<user_id>/follow", view=views.FollowUser.as_view(), name="follow_user"),
     path("<user_id>/unfollow", view=views.UnFollowUser.as_view(), name="unfollow_user"),
-    path("<username>", view=views.UserProfile.as_view(), name="user_profile"),
+
     path("<username>/followers", view=views.UserFollowers.as_view(), name="user_followers"),
-    path("<username>/following", view=views.UserFollowing.as_view(), name="user_following")
+    path("<username>/following", view=views.UserFollowing.as_view(), name="user_following"),
+    path("search", view=views.Search.as_view(), name="search"),
+    path("<username>", view=views.UserProfile.as_view(), name="user_profile")
 
 ]
